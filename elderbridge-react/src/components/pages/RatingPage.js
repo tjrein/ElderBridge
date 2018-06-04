2c77c8e2199f8310de1961636d989fe840a0fe11
tjrein <tomjrein@gmail.com>
2018-06-04 19:11:23 -0400

    added hover effects to rating

--- elderbridge-react/src/components/pages/RatingPage.js
+++ elderbridge-react/src/components/pages/RatingPage.js
@@ -1,29 +1,74 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Form, TextArea, Button, Icon } from 'semantic-ui-react';
 
 class RatingPage extends Component {
+  state = {
+    meh: "grey",
+    frown: "grey",
+    smile: "grey"
+  };
+
+  handleLeave = e => {
+    let key = e.currentTarget.id;
+    this.setState({ [key]: "grey" });
+  }
+
+  handleHover= e => {
+    let key = e.currentTarget.id;
+    let color = {
+      smile: "green",
+      meh: "yellow",
+      frown: "red"
+    }[key];
+
+    this.setState({ [key]: color });
+  }
 
   render() {
-
     return (
       <div>
         <Navbar display_backbutton={true} history={this.props.history}/>
-        <h1> Rating page </h1>
         <div className="ui container">
-        <Grid columns={3} centered>
-          <Grid.Row>
-            <Grid.Column textAlign="center">
-              <i className="fa fa-smile-o fa-5x fa-fw" aria-hidden="true"></i>
-            </Grid.Column>
-            <Grid.Column textAlign="center">
-              <i className="fa fa-smile-o fa-5x fa-fw" aria-hidden="true"></i>
-            </Grid.Column>
-            <Grid.Column textAlign="center">
-              <i className="fa fa-smile-o fa-5x fa-fw" aria-hidden="true"></i>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+          <Grid columns={3} centered>
+            <Grid.Row>
+              <Grid.Column textAlign="center">
+                <Button circular basic
+                  size="massive"
+                  id="smile"
+                  icon={<Icon id="smile-icon" fitted name="smile" className="outline" size="huge" color={this.state.smile} />}
+                  onMouseEnter={this.handleHover}
+                  onMouseLeave={this.handleLeave}
+                />
+              </Grid.Column>
+              <Grid.Column textAlign="center">
+                <Button circular basic
+                  size="massive"
+                  id="meh"
+                  icon={<Icon name="meh" className="outline" size="huge" color={this.state.meh} />}
+                  onMouseEnter={this.handleHover}
+                  onMouseLeave={this.handleLeave}
+                />
+              </Grid.Column>
+              <Grid.Column textAlign="center">
+                <Button circular basic
+                  size="massive"
+                  id="frown"
+                  icon={<Icon name="frown" className="outline" size="huge" color={this.state.frown} />}
+                  onMouseEnter={this.handleHover}
+                  onMouseLeave={this.handleLeave}
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+        <div className="ui container">
+          <Form>
+            <TextArea placeholder='Tell us more' style={{ minHeight: 100 }} />
+            <div className="ui container center aligned">
+              <Button primary size="massive" type='submit'>Submit</Button>
+            </div>
+          </Form>
         </div>
       </div>
     );
